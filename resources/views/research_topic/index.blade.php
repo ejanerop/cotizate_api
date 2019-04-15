632c0db8231ddbf7fd8752e5873041062274434b
eriC <ejanerop97@gmail.com>
2019-04-15 22:39:45 +0200

    Creando vistas de research topic

--- resources/views/research_topic/index.blade.php
+++ resources/views/research_topic/index.blade.php
@@ -1,14 +1,23 @@
-<html>
-    <head>
-        <title>App Name - @yield('title')</title>
-    </head>
-    <body>
-        @section('sidebar')
-            This is the master sidebar.
-        @show
+@extends('layouts.app')
 
-        <div class="container">
-            @yield('content')
-        </div>
-</body>
-</html>
+@section('content')
+
+    <div class="container">
+        <table class="table table-bordered">
+            <tr class="active">
+                <th>Tema de investigación</th>
+                <th>Descripción</th>
+                <th>Cantidad de documentos</th>
+                <th>Acciones</th>
+            </tr>
+            <tr>
+                <td>Lucha Revolucionaria</td>
+                <td>Lorem ipsum dolor sit...</td>
+                <td>15</td>
+                <td> <button class="btn-outline-info">Modificar</button> <button class="btn-outline-danger">Eliminar</button> </td>
+            </tr>
+
+        </table>
+    </div>
+
+@endsection
